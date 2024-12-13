bdbd0c3092e1ab93a3e0ade0f8f4041b2df1edc1
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2024-12-13 15:15:14 +0100

    d1 p2

--- 1/1.zig
+++ 1/1.zig
@@ -4,6 +4,16 @@ const io = std.io;
 const debug = std.debug;
 const ArrayList = std.ArrayList;
 
+fn countOccurences(list: std.ArrayList(i32), target: i32) i32 {
+    var count: i32 = 0;
+    for (list.items) |item| {
+        if (item == target) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 pub fn main() !void {
     const path = "input.txt";
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -13,11 +23,9 @@ pub fn main() !void {
     var file = try fs.cwd().openFile(path, .{ .mode = fs.File.OpenMode.read_only });
     defer file.close();
 
-    // Things are _a lot_ slower if we don't use a BufferedReader
     var buffered = std.io.bufferedReader(file.reader());
     var in_stream = buffered.reader();
 
-    // lines will get read into this
     var arr = ArrayList(u8).init(allocator);
     defer arr.deinit();
 
@@ -37,14 +45,10 @@ pub fn main() !void {
         const second_str = iterator.next() orelse continue;
         const second_num = try std.fmt.parseInt(i32, second_str, 10);
         try list2.append(second_num);
-        std.debug.print("Read: {d} {d}\n", .{ first_num, second_num });
     }
 
-    std.debug.print("Before sorting:\n", .{});
-    std.debug.print("{d}\n", .{list1.items[0]});
     std.mem.sort(i32, list1.items, {}, comptime std.sort.asc(i32));
     std.mem.sort(i32, list2.items, {}, comptime std.sort.asc(i32));
-    std.debug.print("{d}\n", .{list1.items[0]});
 
     if (list1.items.len != list2.items.len) {
         std.debug.print("Lists are not the same length\n", .{});
@@ -53,9 +57,13 @@ pub fn main() !void {
 
     var distance: u32 = 0;
     for (list1.items, 0..) |item, i| {
-        std.debug.print("Index: {d}\n", .{i});
-        std.debug.print("Comparing: {d} {d}\n", .{ item, list2.items[i] });
         distance += @abs(item - list2.items[i]);
     }
     std.debug.print("Distance: {d}\n", .{distance});
+
+    var similarity: i32 = 0;
+    for (list1.items) |item| {
+        similarity += item * countOccurences(list2, item);
+    }
+    std.debug.print("Similarity: {d}\n", .{similarity});
 }
