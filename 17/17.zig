8bfde072ead56af0337f048d505e97a7ee450d3c
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2024-12-17 22:08:13 +0100

    refactor

--- 17/17.zig
+++ 17/17.zig
@@ -1,119 +1,9 @@
 const std = @import("std");
 
-const input_file_name = "input3.txt";
+const input_file_name = "input.txt";
 const input_data = @embedFile(input_file_name);
 
-const Computer = struct {
-    A: u32 = 0,
-    B: u32 = 0,
-    C: u32 = 0,
-
-    opcodes: []u8 = undefined,
-
-    instruction_pointer: usize = 0,
-
-    result: std.ArrayList(u8),
-
-    fn solution(self: *Computer) ![]u8 {
-        var result = std.ArrayList(u8).init(std.heap.page_allocator);
-        defer result.deinit();
-
-        // Iterate through numbers and add commas
-        for (self.result.items, 0..) |num, i| {
-            try std.fmt.format(result.writer(), "{d}", .{num});
-
-            if (i < self.result.items.len - 1) {
-                try result.append(',');
-            }
-        }
-        return result.toOwnedSlice();
-    }
-
-    fn combo_operand_value(self: *Computer, combo_operand: u32) u32 {
-        const value = switch (combo_operand) {
-            0 => 0,
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            4 => self.A,
-            5 => self.B,
-            6 => self.C,
-            else => 0,
-        };
-        return value;
-    }
-
-    fn run_opcode(self: *Computer, opcode: u8, operand: u32) !void {
-        //std.debug.print("opcode: {d}; operand: {d}\n", .{ opcode, operand });
-        const literal = operand;
-        const combo = self.combo_operand_value(operand);
-        //std.debug.print("literal: {d}; combo: {d}\n", .{ literal, combo });
-        switch (opcode) {
-            0 => {
-                // computer.A = std.math.trunc(A / ( 2 ** combo))
-                self.A = @divTrunc(self.A, std.math.pow(u32, 2, combo));
-                self.instruction_pointer += 2;
-            },
-            1 => {
-                // computer.B = computer.B ^ literal
-                self.B = self.B ^ literal;
-                self.instruction_pointer += 2;
-            },
-            2 => {
-                // B = combo % 8
-                self.B = combo % 8;
-                self.instruction_pointer += 2;
-            },
-            3 => {
-                // if A == 0 { pointer += 2 } else { pointer = opcode }
-                if (self.A == 0) {
-                    self.instruction_pointer += 2;
-                } else {
-                    self.instruction_pointer = literal;
-                }
-            },
-            4 => {
-                // B = B ^ C
-                self.B = self.B ^ self.C;
-                self.instruction_pointer += 2;
-            },
-            5 => {
-                // print combo % 8
-                try self.result.append(@as(u8, @intCast(combo % 8)));
-                self.instruction_pointer += 2;
-            },
-            6 => { //
-                // computer.B = std.math.trunc(A / ( 2 ** combo ))
-                self.B = @divTrunc(self.A, std.math.pow(u32, 2, combo));
-                self.instruction_pointer += 2;
-            },
-            7 => {
-                // computer.C = std.math.trunc(A / ( 2 ** combo))
-                self.C = @divTrunc(self.A, std.math.pow(u32, 2, combo));
-                self.instruction_pointer += 2;
-            },
-            else => {},
-        }
-    }
-
-    fn run(self: *Computer) !void {
-        const literal_operand = self.opcodes[self.instruction_pointer];
-        const combo_operand = self.opcodes[self.instruction_pointer + 1];
-        try self.run_opcode(literal_operand, combo_operand);
-    }
-
-    fn run_computer(self: *Computer) ![]u8 {
-        while (self.instruction_pointer < self.opcodes.len) {
-            //std.debug.print("instruction_pointer: {d}\n", .{self.instruction_pointer});
-            try self.run();
-            //std.debug.print("{any}\n", .{self});
-        }
-        //std.debug.print("\nfin\n\n", .{});
-        return self.result.toOwnedSlice();
-    }
-};
-
-fn parse_computer(input: []const u8) !Computer {
+fn parse_computer(input: []const u8) !struct { a: u32, b: u32, c: u32, opcodes: []u8 } {
     var a: u32 = 0;
     var b: u32 = 0;
     var c: u32 = 0;
@@ -155,52 +45,116 @@ fn parse_computer(input: []const u8) !Computer {
         }
         line_no += 1;
     }
-    return Computer{ .A = a, .B = b, .C = c, .opcodes = opcodes, .instruction_pointer = 0, .result = std.ArrayList(u8).init(std.heap.page_allocator) };
+    return .{ .a = a, .b = b, .c = c, .opcodes = opcodes };
+}
+
+fn combo_operand_value(combo_operand: u32, A: u32, B: u32, C: u32) u32 {
+    const value = switch (combo_operand) {
+        0 => 0,
+        1 => 1,
+        2 => 2,
+        3 => 3,
+        4 => A,
+        5 => B,
+        6 => C,
+        else => 0,
+    };
+    return value;
+}
+
+fn run_computer(a: u32, b: u32, c: u32, opcodes: []u8) ![]u8 {
+    var A = a;
+    var B = b;
+    var C = c;
+
+    var result = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer result.deinit();
+
+    var instruction_pointer: usize = 0;
+    while (instruction_pointer < opcodes.len) {
+        const opcode = opcodes[instruction_pointer];
+        const operand = opcodes[instruction_pointer + 1];
+
+        const literal = operand;
+        const combo = combo_operand_value(operand, A, B, C);
+
+        switch (opcode) {
+            0 => {
+                // computer.A = std.math.trunc(A / ( 2 ** combo))
+                A = @divTrunc(A, std.math.pow(u32, 2, combo));
+                instruction_pointer += 2;
+            },
+            1 => {
+                // computer.B = computer.B ^ literal
+                B = B ^ literal;
+                instruction_pointer += 2;
+            },
+            2 => {
+                // B = combo % 8
+                B = combo % 8;
+                instruction_pointer += 2;
+            },
+            3 => {
+                // if A == 0 { pointer += 2 } else { pointer = opcode }
+                if (A == 0) {
+                    instruction_pointer += 2;
+                } else {
+                    instruction_pointer = literal;
+                }
+            },
+            4 => {
+                // B = B ^ C
+                B = B ^ C;
+                instruction_pointer += 2;
+            },
+            5 => {
+                // print combo % 8
+                try result.append(@as(u8, @intCast(combo % 8)));
+                instruction_pointer += 2;
+            },
+            6 => { //
+                // computer.B = std.math.trunc(A / ( 2 ** combo ))
+                B = @divTrunc(A, std.math.pow(u32, 2, combo));
+                instruction_pointer += 2;
+            },
+            7 => {
+                // computer.C = std.math.trunc(A / ( 2 ** combo))
+                C = @divTrunc(A, std.math.pow(u32, 2, combo));
+                instruction_pointer += 2;
+            },
+            else => {},
+        }
+    }
+    return result.toOwnedSlice();
 }
 
 pub fn main() !void {
     var timer = try std.time.Timer.start();
-    //var c = try parse_computer("Register A: 0\nRegister B: 0\nRegister C: 9\n\nProgram: 2,6");
-    //try c.run_computer();
-    //std.debug.print("{any}\n", .{c});
-    //c = try parse_computer("Register A: 10\nRegister B: 0\nRegister C: 0\n\nProgram: 5,0,5,1,5,4");
-    //try c.run_computer();
-    //std.debug.print("{any}\n", .{c});
-    //c = try parse_computer("Register A: 2024\nRegister B: 0\nRegister C: 0\n\nProgram: 0,1,5,4,3,0");
-    //try c.run_computer();
-    //std.debug.print("{any}\n", .{c});
 
-    std.debug.print("{s}\n\n", .{input_data});
-    const computer_clean = try parse_computer(input_data);
-    var computer = computer_clean;
-    //std.debug.print("{any}\n", .{computer});
-    const solution = try computer.run_computer();
-    //std.debug.print("{any}\n", .{computer});
-    std.debug.print("part1 {s}   {d}ms\n", .{ solution, timer.lap() / std.time.ns_per_ms });
+    const computer_data = try parse_computer(input_data);
+    std.debug.print("{any}\n\n", .{computer_data});
 
-    //const target = "2,4,1,1,7,5,1,5,4,2,5,5,0,3,3,0";
-    //const target = "0,3,5,4,3,0";
-    const target: [6]u8 = [_]u8{ 0, 3, 5, 4, 3, 0 };
-    computer = computer_clean;
-    var moving_a: u32 = computer.A;
-    var target_index = target.len;
-    moving_a = 1;
-    computer = computer_clean;
-    computer.A = moving_a;
+    const solution: []const u8 = try run_computer(computer_data.a, computer_data.b, computer_data.c, computer_data.opcodes);
+    std.debug.print("part1 {any}   {d}ms\n", .{ solution, timer.lap() / std.time.ns_per_ms });
+
+    const target: [16]u8 = [_]u8{ 2, 4, 1, 1, 7, 5, 1, 5, 4, 2, 5, 5, 0, 3, 3, 0 };
+    //const target: [6]u8 = [_]u8{ 0, 3, 5, 4, 3, 0 };
     std.debug.print("{any}\n", .{target});
+
+    var moving_a: u32 = 0;
+    var target_index = target.len;
     while (target_index > 0) {
         target_index -= 1;
 
         moving_a <<= 3;
         std.debug.print("target_index: {d} a: {d}\n", .{ target_index, moving_a });
 
-        var soli = try computer.run_computer();
+        var soli = try run_computer(moving_a, computer_data.b, computer_data.c, computer_data.opcodes);
+        //std.debug.print("soli: {any}\n", .{soli});
         while (!std.mem.eql(u8, soli, target[target_index..])) {
-            //std.debug.print("soli: {any} tar: {any} {d}\n", .{ soli, target[target_index..], moving_a });
-            computer.A = moving_a;
-            computer = computer_clean;
             moving_a += 1;
-            soli = try computer.run_computer();
+            soli = try run_computer(moving_a, computer_data.b, computer_data.c, computer_data.opcodes);
+            //std.debug.print("soli: {any} target: {any} a: {d}\n", .{ soli, target[target_index..], moving_a });
         }
     }
 
